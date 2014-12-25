97842e8cf3531f6f6d4786ebefbcccb205eee849
Olexandr Nazarenko <nazarenko@samsonos.com>
2014-12-25 16:46:11 +0200

    added controller

---
+++ src/Controller.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nazarenko
+ * Date: 25.12.2014
+ * Time: 16:23
+ */
+
+namespace samson\cms\web\payment;
+
+/**
+ * Class Controller
+ * @package samson\cms\web\payment
+ * Payment for samsonCMS
+ */
+
+class Controller extends \samson\cms\App
+{
+
+    /** @var string Application identifier */
+    public $id = 'payment';
+    /** @var string Application display name */
+    public $name = 'Оплаты';
+
+}
